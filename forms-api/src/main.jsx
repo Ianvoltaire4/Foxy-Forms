0f81a4c6108566578e420855e2a6f1dcb6010b27
Ian Voltaire <ianvoltaire4@gmail.com>
2023-09-27 14:48:19 -0400

    Fixed main.jsx problem

--- forms-api/src/main.jsx
+++ forms-api/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Forms from './forms.jsx'
+import Forms from './Forms.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
